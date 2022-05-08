dbbd2a50a8bac73540c68a9359bacab87d9cb566
Xiaoran Su <xiaoran.su@proptrack.com>
2022-05-08 10:16:02 +0800

    init

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,4 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CustomerService } from './customer.service';
 import { CustomerDto } from './dto/customer.dto';
 import { ControllerSerializer } from '../decorators/controllerSerializer';
@@ -8,7 +13,8 @@ export class CustomerController {
   constructor(private readonly customerService: CustomerService) {}
 
   @Get()
-  @ControllerSerializer()
+  // @ControllerSerializer()
+  @UseInterceptors(ClassSerializerInterceptor)
   findOne(): any {
     const customer = this.customerService.findOne();
     return new CustomerDto(customer);

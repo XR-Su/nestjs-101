e80423c155819b112866145b141e217b562c6c10
Xiaoran <xiaoran.su@proptrack.com>
2022-05-11 22:07:03 +0800

    order

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,13 @@
 import { Module, MiddlewareConsumer } from '@nestjs/common';
-import { CustomerController } from './customer/customer.controller';
-import { CustomerService } from './customer/customer.service';
-import { LoggerMiddleware } from './middleware/logger.middleware'
+import { LoggerMiddleware } from './middleware/logger.middleware';
+import { CustomerModule } from './customer/customer.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [],
-  controllers: [CustomerController],
-  providers: [CustomerService],
+  imports: [CustomerModule, OrderModule],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(LoggerMiddleware)
-      .forRoutes('*');
+    consumer.apply(LoggerMiddleware).forRoutes('*');
   }
 }
